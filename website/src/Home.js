3642cba99f74b4efebf0253c919f12df78d9ffd2
yxu60101 <93835715+yxu60101@users.noreply.github.com>
2022-06-05 00:08:56 -0400

    main4

--- website/src/Home.js
+++ website/src/Home.js
@@ -1,7 +1,10 @@
+import wtm from "./withthymight.jpg"
+
 const Home = () => {
 
     return ( 
         <div className="home">
+            <img src= { wtm } alt="" />
             <h2>"Whatsoever thy hand findeth to do, do it with thy might;.." <br /> Ecclesiastes 9:10</h2>
         </div>
      );
