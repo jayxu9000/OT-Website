c8a2eb26320673a9dfafdd18de4e8e345bcb51ae
yxu60101 <93835715+yxu60101@users.noreply.github.com>
2022-06-19 23:22:04 -0400

    main 6/19/2022 11:21 PM

--- website/src/Footer.js
+++ website/src/Footer.js
@@ -3,22 +3,28 @@ import COA from './COA.png'
 const Footer = () => {
     return (
         <div className="Footer">
-                <div className="Sections">
-                    <div className="Section1">
-                        <div className="Section1_Title">
-                            <img src= {COA} alt="" />
-                            <h1>Theta Tau</h1>
-                        </div>
-                        <h2>Mu Gamma Chapter</h2>
-                        <h2>University At Buffalo</h2>
+            <div className="Sections">
+                <div className="Section1">
+                    <div className="Section1_Title">
+                        <img src= {COA} alt="" />
+                        <h1>Theta Tau</h1>
                     </div>
-                    <div className="Section2">
-                        <h1>Contacts</h1>
-                        <h2>testing123@gmail.com</h2>
-                        <h2>Mu Gamma Chapter LinkedIn</h2>
-                        <h2>Instagram</h2>
-                    </div>
+                    <h2>Mu Gamma Chapter</h2>
+                    <h2>University At Buffalo</h2>
                 </div>
+                <div className="Section2">
+                    <h1>General</h1>
+                    <h2>National Theta Tau</h2>
+                    <h2>Executive Board</h2>
+                    <h2>Committee Heads</h2>
+                </div>
+                <div className="Section3">
+                    <h1>Contacts</h1>
+                    <h2>testing123@gmail.com</h2>
+                    <h2>Instagram</h2>
+                    <h2>LinkedIn</h2>
+                </div>
+             </div>
         </div>
 
     );
