d51daa074e7b0bd710be61b39de2c9184d5d6476
yxu60101 <93835715+yxu60101@users.noreply.github.com>
2022-06-23 16:26:05 -0400

    main 6/23/2022 4:25 PM

--- website/src/Alumni.js
+++ website/src/Alumni.js
@@ -98,6 +98,317 @@ const Alumni = () => {
                             </div>
                                   
                         </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Joshua Hulburt</p>
+                                <p>Industrial Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Walter Michalski</p>
+                                <p>Civil Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Mathew Montera</p>
+                                <p>Aerospace Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Daniel Petrov</p>
+                                <p>Chemical Engineering BS, 2022</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>David Zhang</p>
+                                <p>Chemical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Hollis Pauquette</p>
+                                <p>Computer Science BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Daniel Brenman</p>
+                                <p>Mechanical &#38; Aerospace Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Trevor de las Alas</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Casey Dolan</p>
+                                <p>Computer Engineering BS, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Christopher Donnelly</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Francis Duffy</p>
+                                <p>Chemical Engineering BS, 2022</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Zeev Elias</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Aaron Gorsline</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Navid Khan</p>
+                                <p>Computer Science BS, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Johnny Liang</p>
+                                <p>Electrical Engineering BS, 202?</p>
+                            </div>
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Kyle Morlock</p>
+                                <p>Mechannical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Isaac Rajan</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Ashley Sanders</p>
+                                <p>Chemical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Benjamin Thide</p>
+                                <p>Mechanical &#38; Aerospace Engineering BS, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Nicholas Bluto</p>
+                                <p>Aerospace Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Victorio Cassela</p>
+                                <p>Mechanical &#38; Aerospace Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Emma Crooks</p>
+                                <p>Industrial Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Justin Diehl</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Lucas Kanestrin</p>
+                                <p>Biomedical Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Jared McMahon</p>
+                                <p>Mechanical &#38; Aerospace Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Margaret Sramek</p>
+                                <p>Environmental Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Carmen Su</p>
+                                <p>Industrial Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Vincent Tricozzi</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Maxwell Bowersox</p>
+                                <p>Mechanical Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Filipe Faria</p>
+                                <p>Civil Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Amy Faville</p>
+                                <p>Mechanical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Alvi Hossain</p>
+                                <p>Mechanical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Maria Kay</p>
+                                <p>Chemical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Eugene Liang</p>
+                                <p>Industrial Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Taylor Marabella</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Michael Morgenthal</p>
+                                <p>Computer Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Jessica Nguyen</p>
+                                <p>Civil Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Harsh Patel</p>
+                                <p>Computer Science BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Jamie Rosner</p>
+                                <p>Electrical Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Devin Wiseman</p>
+                                <p>Chemical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Aaron Cruz</p>
+                                <p>Industrial Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Taha Alam</p>
+                                <p>Computer Science BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Aly Garcia</p>
+                                <p>Chemical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Nicole Gutierrez</p>
+                                <p>Biomedical Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Gifari Hoque</p>
+                                <p>Computational Mathematics BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>John Karst</p>
+                                <p>Electrical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Jonathan Mann</p>
+                                <p>Civil Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Amanda Pasquini</p>
+                                <p>Biomedical Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Wakil Pranto</p>
+                                <p>Civil Engineering, 202?</p>
+                            </div> 
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Luo Qi &#40;Yuki&#41; Cao</p>
+                                <p>Mechanical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>
+                        
+                        <div className="FiveGroup">
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Kelsey Kehoe</p>
+                                <p>Industrial Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Nadia Razick</p>
+                                <p>Mechanical &#38; Aerospace Engineering BS, 202?</p>
+                            </div>
+                            <div className="Profile">
+                                <strong><img src= {PFP} alt="" /></strong>
+                                <p>Troy Killian</p>
+                                <p>Mechanical Engineering BS, 202?</p>
+                            </div>
+                                  
+                        </div>                
+                    
+                        
                     </div>
                 </center>
             </div>
